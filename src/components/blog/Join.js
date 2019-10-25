dfbbd65cd7b68a3ee1b2d2e0c77d62a363cf55a9
Samuel-Chris <46243345+Samuel-Chris@users.noreply.github.com>
2019-10-25 23:15:31 +0200

    Update Join.js

--- src/components/blog/Join.js
+++ src/components/blog/Join.js
@@ -4,9 +4,9 @@ import './blog-styles.scss'
 
 export default function Join() {
     return (
-        <div className="join">
+        <div className="join-us">
             <h3>Join the community!</h3>
-            <span className="join-us-text">Join our Discord community to share knowledge, network and get feedback on your startup</span>
+            <p className="join-us-text">Join our Discord community to share knowledge, network and get feedback on your startup</p>
             <a href="https://discord.gg/r3j7Byh" target="_blank" rel="noopener noreferrer"><Button white width="12rem">Join Us</Button></a>
         </div>
     )
